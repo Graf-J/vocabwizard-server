614cafbf38156e089fbed1b091a1a867126c5911
Graf-J <johannes.graf.1908@gmail.com>
2024-01-13 22:27:09 +0100

    Add Auth-Service Tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,8 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
-import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+import { createMock } from '@golevelup/ts-jest';
+import * as bcrypt from 'bcrypt';
+import { UnauthorizedException } from '@nestjs/common';
 
-const moduleMocker = new ModuleMocker(global);
+jest.mock('bcrypt', () => ({
+  compare: jest.fn(),
+}));
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -11,15 +15,7 @@ describe('AuthService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AuthService],
     })
-      .useMocker((token) => {
-        if (typeof token === 'function') {
-          const mockMetadata = moduleMocker.getMetadata(
-            token,
-          ) as MockFunctionMetadata<any, any>;
-          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
-          return new Mock();
-        }
-      })
+      .useMocker(createMock)
       .compile();
 
     service = module.get<AuthService>(AuthService);
@@ -27,5 +23,32 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('validatePassword', () => {
+    it('should throw exception if bcrypt decides passwords do not match', async () => {
+      (bcrypt.compare as jest.Mock).mockResolvedValue(false);
+
+      const responsePromise = service.validatePassword(
+        'invalidPassword',
+        'passwordHash',
+      );
+
+      await expect(responsePromise).rejects.toThrow(UnauthorizedException);
+      await expect(responsePromise).rejects.toThrow(
+        'Username or Password is not valid',
+      );
+    });
+
+    it('should throw no exception if bcrypt decides passwords do match', async () => {
+      (bcrypt.compare as jest.Mock).mockResolvedValue(true);
+
+      const responsePromise = service.validatePassword(
+        'validPassword',
+        'passwordHash',
+      );
+
+      await expect(responsePromise).resolves.not.toThrow();
+    });
   });
 });
