e046c73e80bebb445083b048babb9e65edad36c4
Graf-J <johannes.graf.1908@gmail.com>
2024-01-21 22:46:25 +0100

    Add CORS

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ import { ImportDeckDto } from './deck/dto/import-deck.dto';
 import { StatDto } from './deck/dto/stat.dto';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   // Add Validation Pipe
   app.useGlobalPipes(new ValidationPipe());
